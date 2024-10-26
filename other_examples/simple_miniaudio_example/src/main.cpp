db657eb7707eba55d217ed810f91aee474676d61
doc1oo <doc100@gmail.com>
2024-10-26 13:47:24 +0900

    ソースコードのフォーマット

--- other_examples/simple_miniaudio_example/src/main.cpp
+++ other_examples/simple_miniaudio_example/src/main.cpp
@@ -3,20 +3,19 @@
 #include <windows.h>
 #include <iostream>
 
-
 double g_phase = 0.0;
 
 // Automatically called by miniaudio when it needs more data for the playback buffer.
-void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+void data_callback(ma_device *pDevice, void *pOutput, const void *pInput, ma_uint32 frameCount)
 {
-    auto output = (ma_float*)pOutput;
+    auto output = (ma_float *)pOutput;
 
     for (int i = 0; i < frameCount; i++)
     {
         float sample = 0.9 * sinf(g_phase);
 
-        output[i * 2 + 0] = sample;     // Left Channel
-		output[i * 2 + 1] = sample;     // Right Channel
+        output[i * 2 + 0] = sample; // Left Channel
+        output[i * 2 + 1] = sample; // Right Channel
 
         g_phase += 0.1;
 
@@ -39,12 +38,13 @@ int main()
     ma_device_init(NULL, &config, &device);
     ma_device_start(&device);
 
-	// Wait 3 seconds -----------------------------
-    for (int i = 0; i < 3; i++) {
+    // Wait 3 seconds -----------------------------
+    for (int i = 0; i < 3; i++)
+    {
         std::cout << "Now playing sine wave... " << i << std::endl;
-		Sleep(1000);
+        Sleep(1000);
     }
-    
-	// Unititialising Audio Device -----------------
+
+    // Unititialising Audio Device -----------------
     ma_device_uninit(&device);
 }
